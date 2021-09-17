97b122bf3a0fa6c7d41e5035881972f362088cc4
Marina Fischer <marinamirandafischer@gmail.com>
2021-09-17 15:18:12 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styled from 'styled-components';
 
-function App() {
+const Site = styled.div `
+    justify-content: center;
+
+`;
+
+const Input = styled.input `
+    widh: 400px;
+    height: 30px;
+    font-size: 16px;
+    padding:10px
+    border: 1px solid black;
+`;
+
+const TituloGrande = styled.h1 `
+    color: pink;
+`;
+
+function App () {
+    const [conta, setConta] = useState();
+    const [gorjeta, setGorjeta] = useState(10);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Site> 
+      <TituloGrande>CALCULADORA DE GORJETA</TituloGrande>
+      <p>Quanto deu sua conta?</p>
+      <Input type="number" value={conta} onChange={(e)=>setConta(e.target.value)}/> 
+      <p>Qual percentual de gorjeta?</p>
+      <Input type="number" value={gorjeta} onChange={(e)=>setGorjeta(e.target.value)}/> 
+      <hr></hr>
+      
+      {conta > 0 &&
+      <>
+        <p>Sub-total R${conta}</p>
+        <p>Gorjeta ({gorjeta}%): R$ {conta*gorjeta/100}</p>
+        <h3> Total: {Number(conta) + (conta*gorjeta/100)} R$ </h3>
+      </>
+      }
+       
+    </Site>
   );
 }
 
